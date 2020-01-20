6c2c3ad7d293ddb62af7401565ec4fae1697ed3d
Giovane Possebon <giovane.possebon@involves.com.br>
2020-01-20 13:22:01 -0300

    removing reactiveswift

--- Package.swift
+++ Package.swift
@@ -5,13 +5,11 @@ let package = Package(
     name: "Moya",
     products: [
         .library(name: "Moya", targets: ["Moya"]),
-        .library(name: "ReactiveMoya", targets: ["ReactiveMoya"]),
         .library(name: "RxMoya", targets: ["RxMoya"])
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "4.1.0")),
         .package(url: "https://github.com/antitypical/Result.git", .upToNextMajor(from: "4.1.0")),
-        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", .upToNextMajor(from: ("5.0.0"))),
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "4.0.0"))
     ],
     targets: [
@@ -20,15 +18,6 @@ let package = Package(
             dependencies: [
                 "Alamofire",
                 "Result"],
-            exclude: [
-                "Tests",
-                "Sources/Supporting Files",
-                "Examples"]),
-        .target(
-            name: "ReactiveMoya",
-            dependencies: [
-                "Moya",
-                "ReactiveSwift"],
             exclude: [
                 "Tests",
                 "Sources/Supporting Files",
